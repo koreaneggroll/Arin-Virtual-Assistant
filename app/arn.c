6151d12596839223491a882ed025278754bbbf36
Arin <72822356+koreaneggroll@users.noreply.github.com>
2021-02-21 11:18:52 +0200

    Added Commands

--- app/arn.c
+++ app/arn.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 
 //DEFINES
@@ -13,15 +14,26 @@
 
 //DATA
 typedef enum{
-    SETPSW,
-    HELLO,
-    TIME,
-    EXIT,
+    Setpsw,
+    Hello,
+    Time,
+    Exit,
 }commands;
+
+
+typedef struct{
+
+    char name[25];
+
+}User;
+typedef User usr;
+
+usr user;
 
 
 //PROTOTYPES
 void prompt();
+void evaluate(char *buffer);
 
 
 
@@ -32,13 +44,80 @@ int main(void){
 
 
 
+void evaluate(char *buffer){
+    if(strcmp(buffer, Time)){
+        //TODO
+    }
+
+    else if(strcmp(buffer, Hello)){
+        //TODO
+    }
+
+    else if(strcmp(buffer, Setpsw)){
+        //TODO
+    }
+
+    else if(strcmp(buffer, Exit)){
+        //TODO
+    }
+}
+
+
+
+//COMMAND FUNCTIONS
+
+//prints out what time and date it is
+void time(){
+
+}
+
+
+
+void hello(){
+
+    if(user.name == NULL){
+        printf("Hello there\n");
+    }
+
+    else{
+        printf("Hello %s\n", user.name);
+    }
+}
+
+
+
+void setpsw(){
+
+}
+
+
+
+void exit(){
+    exit(0);
+}
+
+
+
+
 void prompt(){
     clear();
 
     char *buffer = (char*)malloc(sizeof(char) * 50);
 
+
+    if(buffer == NULL){
+        printf("\n\n\nERR: Couldn't allocate memory to 'buffer'\n\n\n");
+        exit(1);
+    }
+
+
+    //Prompts the user for a command until the user writes 'exit'
     while(true){
+        //Prompt
         printf("arn-virtual-assistant# ");
+        //gets the input and stores it in buffer
         scanf("%s", buffer);
+        //Calls a function to evaluate what it should do based on the buffer
+        evaluate(buffer);
     }
 }
